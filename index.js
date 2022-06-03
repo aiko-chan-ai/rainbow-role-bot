f6b66a80d60388c7678e23eef2947c8202bd8660
March 7th <71698422+aiko-chan-ai@users.noreply.github.com>
2022-06-03 14:46:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Client, Collection } = require('discord.js');
-const index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const index = new Array(18).fill('test');
 const config = require('./config.js');
 const roleColorList = config.color;
 let i = 1;
